0b218f1a1953d305bc60696fa4b55ea33a9dd24a
Benedict <markbenedict98@gmail.com>
2019-12-07 17:47:14 +0800

    Added handler for promise rejections

--- server.js
+++ server.js
@@ -24,4 +24,9 @@ if(process.env.NODE_ENV === 'development'){
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+})
