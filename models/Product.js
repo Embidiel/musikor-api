4eb37272cf065a114ab1cfc054d3cd0940024e2a
Benedict <markbenedict98@gmail.com>
2019-12-09 17:04:03 +0800

    Created a model for Products

---
+++ models/Product.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true
+    },
+    slug: String,
+    tracklist: {
+        type: [String],
+        default: []
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        minlength: [100, 'Description must be more than 100 characters']
+    },
+    stock: {
+        type: Number,
+        required: [true, 'Please enter the number of stocks'],
+    },
+    photos: {
+        type: [String],
+        default: 'no-photo.jpg'
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    releaseDate: {
+        type: Date,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
