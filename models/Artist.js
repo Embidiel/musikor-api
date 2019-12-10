42fc318ce6c2b8e67d36d0a8ea1539510d002e7b
Benedict <markbenedict98@gmail.com>
2019-12-10 16:48:45 +0800

    Made a model for Artist

---
+++ models/Artist.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const ArtistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter an artist name'],
+    },
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter artist description'],
+        minlength: [30, 'Description must be more than 30 characters']
+    },
+    member: {
+        type: [String]
+    },
+    timestamps: true
+});
+
+module.exports = mongoose.model('Artist', ArtistSchema);
